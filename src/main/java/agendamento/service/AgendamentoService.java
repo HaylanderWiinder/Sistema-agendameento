08e51b4903a5b30194ebe9f830f2c3c6d64cd076
Haylander Winder <134789177+HaylanderWiinder@users.noreply.github.com>
2023-07-02 10:24:07 -0300

    Update AgendamentoService.java

--- src/main/java/agendamento/service/AgendamentoService.java
+++ src/main/java/agendamento/service/AgendamentoService.java
@@ -10,24 +10,13 @@ public class AgendamentoService {
         this.agendamentoDAO = new AgendamentoDAO();
     }
 
-    /**
-     * Verifica se há disponibilidade para agendar um horário específico.
-     *
-     * @param horario Horário a ser verificado.
-     * @return true se houver disponibilidade, false caso contrário.
-     */
+   
     public boolean verificarDisponibilidade(String horario) {
         Agendamento agendamento = agendamentoDAO.buscarAgendamento(horario);
         return agendamento == null;
     }
 
-    /**
-     * Agenda um horário para um cliente com um profissional específico, se houver disponibilidade.
-     *
-     * @param horario     Horário a ser agendado.
-     * @param cliente     Nome do cliente.
-     * @param profissional Nome do profissional.
-     */
+   
     public void agendar(String horario, String cliente, String profissional) {
         boolean disponivel = verificarDisponibilidade(horario);
         if (disponivel) {
@@ -39,11 +28,7 @@ public class AgendamentoService {
         }
     }
 
-    /**
-     * Cancela um agendamento existente.
-     *
-     * @param agendamento O agendamento a ser cancelado.
-     */
+    
     public void cancelar(Agendamento agendamento) {
         agendamentoDAO.removerAgendamento(agendamento);
         System.out.println("Agendamento cancelado com sucesso!");
